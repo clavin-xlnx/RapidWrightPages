b22ad998eab3b5bc633e6a65e614a85c63b15250
Chris Lavin <chris.lavin@amd.com>
2024-11-15 17:37:27 -0700

    Updating papers/competition
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>

--- docs/_downloads/db22add9e77242144fc6a54a74550ef6/run_timing.tcl
+++
@@ -1,8 +0,0 @@
-# report to GUI
-report_timing_summary -delay_type min_max -report_unconstrained -check_timing_verbose -max_paths 10 -input_pins -routable_nets -name timing_all
-report_timing -from {*/input0_north_reg*} -delay_type min_max -max_paths 10 -sort_by group -input_pins -name timing_North
-report_timing -from {*/output0_north_reg*} -delay_type min_max -max_paths 10 -sort_by group -input_pins -name timing_North_after
-report_timing -to {*/input0_north_reg*} -delay_type min_max -max_paths 10 -sort_by group -input_pins -name timing_North_before
-report_timing -from {*/input0_south_reg*} -delay_type min_max -max_paths 10 -sort_by group -input_pins -name timing_South
-report_timing -from {*/output0_south_reg*} -delay_type min -max_paths 10 -sort_by group -input_pins -name timing_South_after
-report_timing -to {*/input0_south_reg*} -delay_type min_max -max_paths 10 -sort_by group -input_pins -name timing_South_before
